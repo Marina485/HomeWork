da0cf62d858c7380c2f0bc747065c03d9104e69f
Marina485 <marina_485@mail.ru>
2022-02-15 22:13:01 +0300

    Create HomeWorkApp.java

---
+++ HomeWorkApp.java
@@ -0,0 +1,31 @@
+package ru.homeworkapp;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        System.out.println(range10_20(7,16));
+        positNegatPrint(0);
+        System.out.println(posNeg(0));
+        printStrNum("it turned out", 7);
+    }
+
+     static boolean range10_20(int a, int b) {
+        return a + b >= 10 && a + b <= 20;
+    }
+
+    static void positNegatPrint(int c) {
+        System.out.println(c >= 0 ? "Positive" : "Negative");
+    }
+
+    static boolean posNeg(int e) {
+        return e < 0;
+    }
+
+    static void printStrNum(String str, int n) {
+        for (; n > 0; n--) {
+            System.out.println(str);
+        }
+    }
+}
+
+
+
