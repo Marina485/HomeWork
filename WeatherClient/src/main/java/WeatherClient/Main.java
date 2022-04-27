083092b2dd3f19c6d6dcd2bf8eb4e40343b2afef
Marina485 <marina_485@mail.ru>
2022-04-27 22:55:34 +0300

    Create Main.java

---
+++ WeatherClient/src/main/java/WeatherClient/Main.java
@@ -0,0 +1,9 @@
+package WeatherClient;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+
+        userInterfaceView.runInterface();
+    }
+}
